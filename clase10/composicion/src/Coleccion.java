6d5c8e5497c5580b4a58ca4ea22f7d39157140c4
Diego Serrano <diegojserrano@gmail.com>
2021-08-18 15:33:29 -0300

    Inicio del ejercicio de la clase 11

--- clase10/composicion/src/Coleccion.java
+++ clase10/composicion/src/Coleccion.java
@@ -1,18 +1,32 @@
 public class Coleccion implements Iterable {
 
-    Object v[];
+    Comparable v[];
 
     public Coleccion(int cantidadElementos) {
-        v = new Object[cantidadElementos];
+        v = new Comparable[cantidadElementos];
     }
 
-    public void agregar(Object nuevo) {
+    public void agregar(Comparable nuevo) {
         for(int i = 0; i < v.length; i++)
             if (v[i] == null) {
                 v[i] = nuevo;
                 return;
             }
     }
+
+    // Borre del arreglo el objeto o
+    // buscandolo por valor, con comparable
+    public void borrar(Comparable o) {}
+
+    //Busque el objecto o comparando con compareTo.
+    // Si encuentra un objeto que comparado con o
+    // diga que son iguales, lo retorne
+    // Si no, que retorne null.
+    public Object buscar(Comparable o) { return null; }
+
+    // Lo mismo que buscar pero devolviendo true o false.
+    public boolean existe(Comparable o) { return true; }
+
 
     public IteradorColeccion iterator() {
         IteradorColeccion it = new IteradorColeccion(this);
