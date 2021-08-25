fd1a234b5679382cd5b6797dfd367b0a2143a017
Diego Serrano <diegojserrano@gmail.com>
2021-08-25 17:28:02 -0300

    asdfasdf

---
+++ clase12/src/Principal.java
@@ -0,0 +1,9 @@
+public class Principal {
+    public static void main(String[] args) {
+        int x;
+        float y;
+        boolean b;
+        Integer w = new Integer(2);
+        System.out.println(w);
+    }
+}
---
+++ clase13/junit/src/Pruebas.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Pruebas {
+
+    @Test
+    @DisplayName("Primera prueba")
+    public void prueba1() {
+        Assertions.assertEquals(3, 3);
+    }
+}
