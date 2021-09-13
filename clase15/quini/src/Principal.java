00a0a24828c53cedbf60cf30dd929b17ec2b88dd
Diego Serrano <diegojserrano@gmail.com>
2021-09-13 17:33:07 -0300

    Ejercicio clase 18: menú con programación funcional

---
+++ clase15/quini/src/Principal.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        Map<Integer, Apuesta> m = new TreeMap<>();
+
+        m.put(53930,new Apuesta(new HashSet<>()));
+
+        Set<Integer> sorteados = new HashSet<>();
+        sorteados.add(Random)
+
+        for(Apuesta a: m.values())
+            if (a.acerto4(sorteados))
+                System.out.println(a);
+
+    }
+}
